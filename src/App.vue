438bc53dc36f3d2b07b67366d4faf42c73b0cae8
fournier_axe <axel.fournier@supelec.fr>
2020-04-16 22:50:26 +0200

    start navbar + style

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <navbar />
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+
+import Navbar from '@/components/Navbar.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Navbar,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import '@/styles/bulma-theme.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ece8e1;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="../assets/valorant-callouts-logo.svg" width="150" height="36">
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+        <a class="navbar-item">
+          Learn
+        </a>
+        <a class="navbar-item">
+          Play
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Navbar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  .navbar-brand {
+    border-right: solid 2px $red;
+    .navbar-item {
+      img {
+        max-height: 2.5rem;
+      }
+    }
+  }
+</style>
