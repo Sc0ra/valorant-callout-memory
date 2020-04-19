2a68e6de9fa87cbda7c7828b9752d568ad4e661f
fournier_axe <axel.fournier@supelec.fr>
2020-04-19 22:20:41 +0200

    change api baseUrl

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  baseURL: process.env.BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
